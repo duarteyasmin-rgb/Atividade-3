3ed0e6a8fb291b8ff4092d84f0862c29bb81ebdb
duarteyasmin-rgb <duarte.yasmin@uni9.edu.br>
2026-06-03 02:09:45 +0000

    Adicionar validacao.html

---
+++ wwwroot/wwwroot/validacao.js
@@ -0,0 +1,31 @@
+const formulario = document.getElementById("formulario");
+
+formulario.addEventListener("submit", function(event) {
+
+    const nome = document.getElementById("nomes").value;
+    const email = document.getElementById("emails").value;
+   const idade = document.getElementById("idades").value;
+
+    const erro = document.getElementById("erro");
+
+    erro.textContent = "";
+
+    if (nome.trim() === "") {
+        event.preventDefault();
+        erro.textContent = "O nome não pode estar vazio.";
+        return;
+    }
+
+    if (!email.includes("@")) {
+        event.preventDefault();
+        erro.textContent = "Email inválido.";
+        return;
+    }
+
+    if (idade <= 0) {
+        event.preventDefault();
+        erro.textContent = "A idade deve ser maior que zero.";
+        return;
+    }
+
+});
